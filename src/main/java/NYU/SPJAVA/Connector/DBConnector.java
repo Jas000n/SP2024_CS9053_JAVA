93b189979f41e7c4a30c69fdccdc7aa3a6364220
Rafik Saad <rs7983@nyu.edu>
2024-04-14 22:55:48 -0400

    added DB Connector

--- src/main/java/NYU/SPJAVA/Connector/DBConnector.java
+++ src/main/java/NYU/SPJAVA/Connector/DBConnector.java
@@ -1,4 +1,38 @@
 package NYU.SPJAVA.Connector;
 
+import NYU.SPJAVA.utils.Property;
+import NYU.SPJAVA.utils.Property.CONF;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBConnector {
+	private Connection conn = null;
+	
+	public void connect() throws Exception {
+		// load driver
+		
+		String url = Property.get(CONF.DB_URL);
+		String user = Property.get(CONF.DB_USER);
+		String pwd = Property.get(CONF.DB_PWD);
+		
+		try {
+			// load MySQL Driver (generally unnecessary)
+			Class.forName(Property.get(CONF.DB_DRIVER));
+			
+			// connect
+			System.out.println("testing connection to database");
+			conn = DriverManager.getConnection(url, user, pwd);
+			System.out.println("connection successful!");
+			
+		} catch (Exception e) {
+			System.out.println("connection failed!");
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		DBConnector db = new DBConnector();
+		db.connect();
+	}
 }
