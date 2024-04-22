cc5d67c2aac9f7d79984569ccc6a767a92ac5245
Rafik Saad <rs7983@nyu.edu>
2024-04-21 20:24:25 -0400

    game db connector WIP

--- src/main/java/NYU/SPJAVA/Connector/GameDBConnector.java
+++ src/main/java/NYU/SPJAVA/Connector/GameDBConnector.java
@@ -1,20 +1,66 @@
 package NYU.SPJAVA.Connector;
 
+import NYU.SPJAVA.DBEntity.Game;
 import NYU.SPJAVA.utils.Response;
+import NYU.SPJAVA.utils.Response.ResponseCode;
 
 public class GameDBConnector {
-    //create a single game, passing in a game entity, with only some of the field populated
-    //with word_id, mode, status, time_limit,creator_id,created, started
-    //return a game object with every field populated
-    public Response createSingleGame(){
-        return null;
 
-    }
-    //change the data in  a single game, passing in a complete game entity
-    //finding the corresponding record according to primary key and update record
-    //return a boolean
-    public Response updateSingleGame(){
-        return null;
-    }
+	private Game createSingleGame(Game game) throws Exception {
+		return null;
+	}
 
+	private Game createMultiGame(Game game) throws Exception {
+		return null;
+	}
+
+	private void updateSingleGame(Game game) throws Exception {
+		;
+	}
+
+	private void updateMultiGame(Game game) throws Exception {
+		;
+	}
+
+	// create a single game, passing in a game entity, with only some of the field
+	// populated
+	// with word_id, mode, status, time_limit,creator_id,created, started
+	// return a game object with every field populated
+	public Response createGame(Game game) {
+		Game newGame;
+		try {
+			if (game.getMode() == 'S') {
+				newGame = createSingleGame(game);
+			} else {
+				// TODO: implement multi game
+				newGame = createMultiGame(game);
+			}
+			String msg = String.format("Game created successfully, id %d", newGame.getGameID());
+			return new Response(ResponseCode.SUCCESS, msg, null, newGame);
+		} catch (Exception ex) {
+			String msg = "Failed to create game";
+			return new Response(ResponseCode.FAILED, msg, ex, null);
+		}
+
+	}
+
+	// change the data in a single game, passing in a complete game entity
+	// finding the corresponding record according to primary key and update record
+	// return a boolean
+	public Response updateGame(Game game) {
+		try {
+			if (game.getMode() == 'S') {
+				updateSingleGame(game);
+			} else {
+				//TODO: implement MultiGame
+				updateMultiGame(game);
+			}
+			String msg = "Game updated successfully";
+			return new Response(ResponseCode.SUCCESS, msg, null, null);
+		} catch (Exception ex) {
+			String msg = "Failed to update game";
+			return new Response(ResponseCode.FAILED, msg, ex, null);
+		}
+
+	}
 }
--- src/main/java/NYU/SPJAVA/DBEntity/Game.java
+++ src/main/java/NYU/SPJAVA/DBEntity/Game.java
@@ -1,5 +1,63 @@
 package NYU.SPJAVA.DBEntity;
 
+import org.joda.time.LocalDateTime;
+
 public class Game {
+	private final Integer gameID;
+	private final Word word;
+	private final char mode;
+	private char status;
+	private final int timeLimit;
+	private final Player creator;
+	private final int capacity;
+	private final LocalDateTime created;
+	private LocalDateTime started;
+	private LocalDateTime ended;
+
+	public Game(Integer gameID, Word word, char mode, char status, int timeLimit, Player creator, int capacity,
+			LocalDateTime created, LocalDateTime started, LocalDateTime ended) {
+		this.gameID = gameID;
+		this.word = word;
+		this.mode = mode;
+		this.status = status;
+		this.timeLimit = timeLimit;
+		this.creator = creator;
+		this.capacity = capacity;
+		this.created = created;
+		this.started = started;
+		this.ended = ended;
+	}
+
+	public Game(Word word, Player creator) {
+		// minimal game creations
+		this.word = word;
+		this.creator = creator;
+		
+		// defaults
+		this.gameID = null;
+		this.mode = 'S';
+		this.status = 'N';
+		this.timeLimit = 180;
+		this.capacity = 1;
+		this.created = new LocalDateTime();
+	}
+	
+	public Integer getGameID() {
+		return this.gameID;
+	}
+	
+	public char getMode() {
+		return this.mode;
+	}
+	
+	@Override
+	public boolean equals(Object other) {
+		if (other instanceof Game) {
+			Game that = (Game)other;
+			return this.gameID == that.gameID;
+		} else {
+			return false;
+		}
+	}
 
 }
